a52dbd09d062ebb6848a34102504a73d41e320e6
Jos Dehaes <jos.dehaes@gmail.com>
2023-01-31 16:13:42 +0100

    format

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ const NODES: usize = 4;
 async fn main() -> Result<(), eyre::Report> {
     let block_sync_params = config::BlockSynchronizerParameters::default();
     let consensus_api_grpc = config::ConsensusAPIGrpcParameters {
-        socket_addr : Multiaddr::from_str("/ip4/0.0.0.0/tcp/0/http").unwrap(),
+        socket_addr: Multiaddr::from_str("/ip4/0.0.0.0/tcp/0/http").unwrap(),
         get_collections_timeout: Duration::from_secs(5000),
         remove_collections_timeout: Duration::from_secs(5000),
     };
@@ -56,17 +56,17 @@ async fn main() -> Result<(), eyre::Report> {
     for _ in 0..NODES {
         let primary_keypair: AuthorityKeyPair = get_key_pair_from_rng(&mut rand::rngs::OsRng).1;
         primary_keys.push(primary_keypair);
-    };
+    }
     let mut network_keys = Vec::new();
     for _ in 0..NODES {
         let network_keypair: NetworkKeyPair = get_key_pair_from_rng(&mut rand::rngs::OsRng).1;
         network_keys.push(network_keypair);
-    };
+    }
     let mut worker_keys = Vec::new();
     for _ in 0..NODES {
         let worker_keypair: NetworkKeyPair = get_key_pair_from_rng(&mut rand::rngs::OsRng).1;
         worker_keys.push(worker_keypair);
-    };
+    }
 
     // create the committee
     let c = create_committee(&primary_keys, &network_keys);
@@ -77,7 +77,18 @@ async fn main() -> Result<(), eyre::Report> {
     let worker_cache = Arc::new(ArcSwap::from_pointee(w));
 
     for id in 0..NODES {
-        let (primary, worker) = start_node(id as u32, primary_keys.remove(id), network_keys.remove(id), worker_keys.remove(id), parameters.clone(), store.clone(), registry_service.clone(), committee.clone(), worker_cache.clone()).await?;
+        let (primary, worker) = start_node(
+            id as u32,
+            primary_keys.remove(id),
+            network_keys.remove(id),
+            worker_keys.remove(id),
+            parameters.clone(),
+            store.clone(),
+            registry_service.clone(),
+            committee.clone(),
+            worker_cache.clone(),
+        )
+        .await?;
         primary.wait().await;
         worker.wait().await;
     }
@@ -85,7 +96,17 @@ async fn main() -> Result<(), eyre::Report> {
     Ok(())
 }
 
-async fn start_node(id: u32, primary_keypair: AuthorityKeyPair, network_keypair: NetworkKeyPair, worker_keypair: NetworkKeyPair, parameters: Parameters, store: NodeStorage, registry_service: RegistryService, committee: Arc<ArcSwap<Committee>>, worker_cache: Arc<ArcSwap<WorkerCache>>) -> Result<(PrimaryNode, WorkerNode), eyre::Report> {
+async fn start_node(
+    id: u32,
+    primary_keypair: AuthorityKeyPair,
+    network_keypair: NetworkKeyPair,
+    worker_keypair: NetworkKeyPair,
+    parameters: Parameters,
+    store: NodeStorage,
+    registry_service: RegistryService,
+    committee: Arc<ArcSwap<Committee>>,
+    worker_cache: Arc<ArcSwap<WorkerCache>>,
+) -> Result<(PrimaryNode, WorkerNode), eyre::Report> {
     let (_tx_transaction_confirmation, _rx_transaction_confirmation) = channel(100);
 
     let worker = WorkerNode::new(id, parameters.clone(), registry_service.clone());
@@ -116,7 +137,10 @@ async fn start_node(id: u32, primary_keypair: AuthorityKeyPair, network_keypair:
     Ok((primary, worker))
 }
 
-fn create_committee(primary_keys: &Vec<BLS12381KeyPair>, network_keys: &Vec<Ed25519KeyPair>) -> Committee {
+fn create_committee(
+    primary_keys: &Vec<BLS12381KeyPair>,
+    network_keys: &Vec<Ed25519KeyPair>,
+) -> Committee {
     let mut authorities = BTreeMap::<PublicKey, Authority>::new();
     let start_port = 3000usize;
     for i in 0..NODES {
@@ -130,17 +154,30 @@ fn create_committee(primary_keys: &Vec<BLS12381KeyPair>, network_keys: &Vec<Ed25
             },
         );
     }
-    Committee { authorities, epoch: 0 }
+    Committee {
+        authorities,
+        epoch: 0,
+    }
 }
 
-fn create_worker_cache(primary_keys: &Vec<BLS12381KeyPair>, worker_keys: &Vec<Ed25519KeyPair>) -> WorkerCache {
+fn create_worker_cache(
+    primary_keys: &Vec<BLS12381KeyPair>,
+    worker_keys: &Vec<Ed25519KeyPair>,
+) -> WorkerCache {
     let mut workers = BTreeMap::<PublicKey, WorkerIndex>::new();
     let start_port = 3008usize;
     for i in 0..NODES {
         let transactions = format!("/ip4/127.0.0.1/tcp/{}/http", start_port + i + 1);
         let worker = format!("/ip4/127.0.0.1/udp/{}", start_port + i);
         let mut worker_info = BTreeMap::<WorkerId, WorkerInfo>::new();
-        worker_info.insert(0, WorkerInfo {name: worker_keys.get(i).unwrap().public().clone(), transactions: Multiaddr::from_str(transactions.as_str()).unwrap(), worker_address: Multiaddr::from_str(worker.as_str()).unwrap()});
+        worker_info.insert(
+            0,
+            WorkerInfo {
+                name: worker_keys.get(i).unwrap().public().clone(),
+                transactions: Multiaddr::from_str(transactions.as_str()).unwrap(),
+                worker_address: Multiaddr::from_str(worker.as_str()).unwrap(),
+            },
+        );
         let worker_index = WorkerIndex(worker_info);
         workers.insert(primary_keys.get(i).unwrap().public().clone(), worker_index);
     }
